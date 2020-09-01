095fbc2e46885a3972d3c6d26c35f207211c1592
cohix <connorjhicks@gmail.com>
2020-09-01 10:35:06 -0400

    app_meta -> app

--- vlog/structuredlog.go
+++ vlog/structuredlog.go
@@ -6,6 +6,6 @@ type structuredLog struct {
 	LogMessage string      `json:"log_message"`
 	Timestamp  time.Time   `json:"timestamp"`
 	Level      int         `json:"level"`
-	AppMeta    interface{} `json:"app_meta,omitempty"`
+	AppMeta    interface{} `json:"app,omitempty"`
 	ScopeMeta  interface{} `json:"scope,omitempty"`
 }
