4dc846c04a5388d972c40af4e87ced08ea3c21ef
Gabor Javorszky <gabor@suborbital.dev>
2022-04-14 08:25:37 +0100

    [lintfix] gci

--- vk/response.go
+++ vk/response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/pkg/errors"
+
 	"github.com/suborbital/vektor/vlog"
 )
 
--- vk/router.go
+++ vk/router.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+
 	"github.com/suborbital/vektor/vlog"
 )
 
