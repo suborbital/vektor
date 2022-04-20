76f198af05d5954262955ecf32d1f1f6b133ab7c
Gabor Javorszky <gabor@suborbital.dev>
2022-04-20 13:09:36 +0100

    Add even more robust test cases

--- vk/test/middleware_test.go
+++ vk/test/middleware_test.go
@@ -18,39 +18,34 @@ const testCTXKey = "middlewareTesting"
 
 func TestMiddlewares(t *testing.T) {
 	// Set up mocks, expectations, and return values.
-	before := &mocks.MiddlewareTester{}
-	before.EXPECT().JobInMiddleware("before").Return(" [ beforeMW ran ] ").Times(4)
-
-	before2 := &mocks.MiddlewareTester{}
-	before2.EXPECT().JobInMiddleware("before").Return(" [ beforeMWII ran ] ").Times(2)
-
-	after := &mocks.MiddlewareTester{}
-	after.EXPECT().JobInMiddleware("after").Return(" [ afterMW ran ] ").Times(4)
-
-	central := &mocks.MiddlewareTester{}
-	central.EXPECT().JobInMiddleware("central").Return(" [ groupMW ran ] ").Times(1)
+	mockMW := &mocks.MiddlewareTester{}
+	mockMW.EXPECT().JobInMiddleware("before").Return(" [ beforeMW ran ] ").Times(4)
+	mockMW.EXPECT().JobInMiddleware("beforeFlip").Return(" [ beforeFlipMW ran ] ").Times(2)
+	mockMW.EXPECT().JobInMiddleware("after").Return(" [ afterMW ran ] ").Times(4)
+	mockMW.EXPECT().JobInMiddleware("afterFlip").Return(" [ afterFlipMW ran ] ").Times(2)
+	mockMW.EXPECT().JobInMiddleware("centralBefore").Return(" [ centralBeforeMW ran ] ").Times(2)
+	mockMW.EXPECT().JobInMiddleware("centralAfter").Return(" [ centralAfterMW ran ] ").Times(2)
+	mockMW.EXPECT().JobInMiddleware("centralBeforeFlip").Return(" [ centralBeforeFlipMW ran ] ").Times(2)
+	mockMW.EXPECT().JobInMiddleware("centralAfterFlip").Return(" [ centralAfterFlipMW ran ] ").Times(2)
 
 	// Set up handlers with the mocked middleware functions in place.
 	beforeHandler := vk.WrapMiddleware([]vk.Middleware{
-		mwBefore(t, before),
-		mwBefore(t, before2),
-		mwAfter(t, after),
+		mwBefore(t, mockMW, "before"),
+		mwBefore(t, mockMW, "beforeFlip"),
+		mwAfter(t, mockMW, "after"),
+		mwAfter(t, mockMW, "afterFlip"),
 	}, ctxReturnHandler)
 
 	beforeFlipHandler := vk.WrapMiddleware([]vk.Middleware{
-		mwBefore(t, before2),
-		mwBefore(t, before),
-		mwAfter(t, after),
-	}, ctxReturnHandler)
-
-	afterHandler := vk.WrapMiddleware([]vk.Middleware{
-		mwAfter(t, after),
-		mwBefore(t, before),
+		mwBefore(t, mockMW, "beforeFlip"), // these two are flipped compared to the beforeHandler
+		mwBefore(t, mockMW, "before"),
+		mwAfter(t, mockMW, "afterFlip"),
+		mwAfter(t, mockMW, "after"),
 	}, ctxReturnHandler)
 
 	centralHandler := vk.WrapMiddleware([]vk.Middleware{
-		mwAfter(t, after),
-		mwBefore(t, before),
+		mwAfter(t, mockMW, "after"),
+		mwBefore(t, mockMW, "before"),
 	}, ctxReturnHandler)
 
 	// Set up a log for the server to use.
@@ -61,14 +56,27 @@ func TestMiddlewares(t *testing.T) {
 		vk.UseLogger(logger),
 	)
 
-	g := vk.Group("/b").Middleware(mwGroup(t, central))
+	g := vk.Group("/b").Middleware(
+		mwBefore(t, mockMW, "centralBefore"),
+		mwBefore(t, mockMW, "centralBeforeFlip"),
+		mwAfter(t, mockMW, "centralAfter"),
+		mwAfter(t, mockMW, "centralAfterFlip"),
+	)
 	g.GET("/central", centralHandler)
+
+	g2 := vk.Group("/c").Middleware(
+		mwBefore(t, mockMW, "centralBeforeFlip"),
+		mwBefore(t, mockMW, "centralBefore"),
+		mwAfter(t, mockMW, "centralAfterFlip"),
+		mwAfter(t, mockMW, "centralAfter"),
+	)
+	g2.GET("/central", centralHandler)
 
 	// Attach the handlers from above to the server.
 	server.GET("/before", beforeHandler)
 	server.GET("/beforeflip", beforeFlipHandler)
-	server.GET("/after", afterHandler)
 	server.AddGroup(g)
+	server.AddGroup(g2)
 
 	// Create a new vtest based on the server with the handlers.
 	vt := vtest.New(server)
@@ -80,43 +88,40 @@ func TestMiddlewares(t *testing.T) {
 	rFlip, err := http.NewRequest(http.MethodGet, "/beforeflip", nil)
 	require.NoError(t, err)
 
-	r2, err := http.NewRequest(http.MethodGet, "/after", nil)
+	rc, err := http.NewRequest(http.MethodGet, "/b/central", nil)
 	require.NoError(t, err)
 
-	rc, err := http.NewRequest(http.MethodGet, "/b/central", nil)
+	rcFlip, err := http.NewRequest(http.MethodGet, "/c/central", nil)
 	require.NoError(t, err)
 
 	t.Run("before", func(t *testing.T) {
 		// Send the requests.
-		resp1 := vt.Do(r, t)
-		assert.Equal(t, " [ beforeMW ran ]  [ beforeMWII ran ]  [ handler ran at path /before ]  [ afterMW ran ] ", string(resp1.Body))
-		assert.Equal(t, 200, resp1.Status)
+		response := vt.Do(r, t)
+		assert.Equal(t, " [ beforeMW ran ]  [ beforeFlipMW ran ]  [ handler ran at path /before ]  [ afterFlipMW ran ]  [ afterMW ran ] ", string(response.Body))
+		assert.Equal(t, 200, response.Status)
 	})
 
 	t.Run("before flip", func(t *testing.T) {
 		// Send the requests.
-		respFlip := vt.Do(rFlip, t)
-		assert.Equal(t, " [ beforeMWII ran ]  [ beforeMW ran ]  [ handler ran at path /beforeflip ]  [ afterMW ran ] ", string(respFlip.Body))
-		assert.Equal(t, 200, respFlip.Status)
-	})
-
-	t.Run("after", func(t *testing.T) {
-		resp2 := vt.Do(r2, t)
-		assert.Equal(t, " [ beforeMW ran ]  [ handler ran at path /after ]  [ afterMW ran ] ", string(resp2.Body))
-		assert.Equal(t, 200, resp2.Status)
+		response := vt.Do(rFlip, t)
+		assert.Equal(t, " [ beforeFlipMW ran ]  [ beforeMW ran ]  [ handler ran at path /beforeflip ]  [ afterMW ran ]  [ afterFlipMW ran ] ", string(response.Body))
+		assert.Equal(t, 200, response.Status)
 	})
 
 	t.Run("central", func(t *testing.T) {
-		resp3 := vt.Do(rc, t)
-		assert.Equal(t, " [ beforeMW ran ]  [ handler ran at path /b/central ]  [ afterMW ran ]  [ groupMW ran ] ", string(resp3.Body))
-		assert.Equal(t, 200, resp3.Status)
+		response := vt.Do(rc, t)
+		assert.Equal(t, " [ centralBeforeMW ran ]  [ centralBeforeFlipMW ran ]  [ beforeMW ran ]  [ handler ran at path /b/central ]  [ afterMW ran ]  [ centralAfterFlipMW ran ]  [ centralAfterMW ran ] ", string(response.Body))
+		assert.Equal(t, 200, response.Status)
+	})
+
+	t.Run("central flip", func(t *testing.T) {
+		response := vt.Do(rcFlip, t)
+		assert.Equal(t, " [ centralBeforeFlipMW ran ]  [ centralBeforeMW ran ]  [ beforeMW ran ]  [ handler ran at path /c/central ]  [ afterMW ran ]  [ centralAfterMW ran ]  [ centralAfterFlipMW ran ] ", string(response.Body))
+		assert.Equal(t, 200, response.Status)
 	})
 
 	// Check whether the middleware functions were called enough times.
-	before.AssertExpectations(t)
-	before2.AssertExpectations(t)
-	after.AssertExpectations(t)
-	central.AssertExpectations(t)
+	mockMW.AssertExpectations(t)
 }
 
 func ctxReturnHandler(r *http.Request, c *vk.Ctx) (interface{}, error) {
@@ -126,14 +131,14 @@ func ctxReturnHandler(r *http.Request, c *vk.Ctx) (interface{}, error) {
 		beforeMWFromCTX = ctxValue.(string)
 	}
 
-	return fmt.Sprintf("%s [ handler ran at path %s ]", beforeMWFromCTX, r.URL.Path), nil
+	return fmt.Sprintf("%s [ handler ran at path %s ] ", beforeMWFromCTX, r.URL.Path), nil
 }
 
-func mwBefore(t *testing.T, testerMW MiddlewareTester) vk.Middleware {
+func mwBefore(t *testing.T, testerMW MiddlewareTester, input string) vk.Middleware {
 	t.Helper()
 	m := func(handler vk.HandlerFunc) vk.HandlerFunc {
 		f := func(r *http.Request, ctx *vk.Ctx) (iFace interface{}, err error) {
-			mwResult := testerMW.JobInMiddleware("before")
+			mwResult := testerMW.JobInMiddleware(input)
 
 			var storedValue string
 			v := ctx.Get(testCTXKey)
@@ -157,39 +162,14 @@ func mwBefore(t *testing.T, testerMW MiddlewareTester) vk.Middleware {
 	return m
 }
 
-func mwAfter(t *testing.T, testerMW MiddlewareTester) vk.Middleware {
+func mwAfter(t *testing.T, testerMW MiddlewareTester, input string) vk.Middleware {
 	t.Helper()
 	m := func(handler vk.HandlerFunc) vk.HandlerFunc {
 		f := func(r *http.Request, ctx *vk.Ctx) (iFace interface{}, err error) {
 			iFace, err = handler(r, ctx)
 
 			responseBody := ""
-			mwResult := testerMW.JobInMiddleware("after")
-
-			switch v := iFace.(type) {
-			case string:
-				responseBody = v + " " + mwResult
-			default:
-				return nil, vk.Err(http.StatusInternalServerError, "response from handler should have been string, got something different")
-			}
-
-			return responseBody, err
-		}
-
-		return f
-	}
-
-	return m
-}
-
-func mwGroup(t *testing.T, testerMW MiddlewareTester) vk.Middleware {
-	t.Helper()
-	m := func(handler vk.HandlerFunc) vk.HandlerFunc {
-		f := func(r *http.Request, ctx *vk.Ctx) (iFace interface{}, err error) {
-			iFace, err = handler(r, ctx)
-
-			responseBody := ""
-			mwResult := testerMW.JobInMiddleware("central")
+			mwResult := testerMW.JobInMiddleware(input)
 
 			switch v := iFace.(type) {
 			case string:
