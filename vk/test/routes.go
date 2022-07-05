4d6ca6d1d6b6484e4268154979bdf8dfbaebf326
Gabor Javorszky <gabor@suborbital.dev>
2022-07-05 11:40:17 +0100

    Rework some test middlewares and adjust test func sigs

--- vk/test/routes.go
+++ vk/test/routes.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+
 	"github.com/suborbital/vektor/vk"
 )
 
@@ -33,36 +34,36 @@ func AddRoutes(server *vk.Server) {
 }
 
 // HandleFound returns 200
-func HandleFound(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+func HandleFound(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx) (interface{}, error) {
 	ctx.Log.Info("found!")
 
 	return vk.R(200, "gotcha"), nil
 }
 
 // HandleNotFound returns 404
-func HandleNotFound(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+func HandleNotFound(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx) (interface{}, error) {
 	return nil, vk.E(http.StatusNotFound, "Not Found")
 }
 
 // HandleMe handles Me requests
-func HandleMe(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+func HandleMe(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx) (interface{}, error) {
 	return vk.R(200, struct{ Me string }{Me: "mario"}), nil
 }
 
 // HandleYou handles You requests
-func HandleYou(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+func HandleYou(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx) (interface{}, error) {
 	ctx.Log.Info("calling you!")
 
 	return vk.R(201, "created, I guess"), nil
 }
 
 // HandleBadMistake handles a bad mistake
-func HandleBadMistake(r *http.Request, ctx *vk.Ctx) (interface{}, error) {
+func HandleBadMistake(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx) (interface{}, error) {
 	return nil, errors.New("this is a bad idea")
 }
 
 // HandleSock hands Sock requests
-func HandleSock(r *http.Request, ctx *vk.Ctx, conn *websocket.Conn) error {
+func HandleSock(w http.ResponseWriter, r *http.Request, ctx *vk.Ctx, conn *websocket.Conn) error {
 	return nil
 }
 
