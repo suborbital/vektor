efa5710bb92c945fc5f2c1cc387e8e211b295e44
Gabor Javorszky <gabor@suborbital.dev>
2022-07-05 20:12:53 +0100

    Add respondWeb function

--- vk/response.go
+++ vk/response.go
@@ -1,10 +1,12 @@
 package vk
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 
 	"github.com/pkg/errors"
+
 	"github.com/suborbital/vektor/vlog"
 )
 
@@ -74,4 +76,32 @@ func responseOrOtherToBytes(l *vlog.Logger, data interface{}) (int, []byte, cont
 	}
 
 	return statusCode, json, contentTypeJSON
+}
+
+// RespondWeb converts a Go value to JSON and sends it to the client.
+func RespondWeb(ctx context.Context, w http.ResponseWriter, data any, statusCode int) error {
+	// If there is nothing to marshal then set status code and return.
+	if statusCode == http.StatusNoContent {
+		w.WriteHeader(statusCode)
+		return nil
+	}
+
+	// Convert the response value to JSON.
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Set the content type and headers once we know marshaling has succeeded.
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the status code to the response.
+	w.WriteHeader(statusCode)
+
+	// Send the result back to the client.
+	if _, err := w.Write(jsonData); err != nil {
+		return err
+	}
+
+	return nil
 }
